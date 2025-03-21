c2b794d49d9d3dc8c1d70fdaac8d79bddfb3e58b
rfitzger <robert.fitzgerald@nrel.gov>
2025-03-21 11:46:44 -0600

    update to compass 0.9.1

--- rust/bambam/src/model/transit_old/schedule_ops.rs
+++ rust/bambam/src/model/transit_old/schedule_ops.rs
@@ -1,15 +1,27 @@
+use std::borrow::Cow;
+
 use chrono::{DateTime, Duration, Utc};
-use routee_compass_core::model::unit::{Time, TimeUnit};
+use routee_compass_core::model::unit::{AsF64, Convert, Time, TimeUnit, UnitError};
+
+use super::schedule_error::ScheduleError;
 
 /// adds time to a datetime. performs operation at the seconds resolution, so any sub-second
 /// increments will be rounded off.
-pub fn add_delta(datetime: DateTime<Utc>, time: Time, time_unit: TimeUnit) -> DateTime<Utc> {
-    // let time_sec = time_unit.convert(&time, &TimeUnit::Seconds).to_f64() as i64;
-    // let duration = Duration::seconds(time_sec);
+pub fn add_delta(
+    datetime: DateTime<Utc>,
+    time: Time,
+    time_unit: TimeUnit,
+) -> Result<DateTime<Utc>, ScheduleError> {
+    let mut t_convert = Cow::Owned(time);
+    time_unit
+        .convert(&mut t_convert, &TimeUnit::Seconds)
+        .map_err(|_| ScheduleError::AddTimeToDateTimeError(time, time_unit, datetime))?;
+    let time_sec = t_convert.as_f64() as i64;
+    let duration = Duration::seconds(time_sec);
 
     // .ok_or_else(|| ScheduleError::AddTimeToDateTimeError(time, time_unit, datetime));
-    // datetime + duration
-    todo!("update use of units module")
+    let with_delta = datetime + duration;
+    Ok(with_delta)
 }
 
 #[cfg(test)]
@@ -24,7 +36,7 @@ mod test {
         let datetime = Utc.with_ymd_and_hms(2024, 1, 22, 12, 0, 0).unwrap();
         let time = Time::from(3600.0);
         let time_unit = TimeUnit::Seconds;
-        let result = add_delta(datetime, time, time_unit);
+        let result = add_delta(datetime, time, time_unit).unwrap();
         assert_eq!(result.hour(), 13);
     }
 }
