60d58e35e074866b71b81e5e391122212501e012
rfitzger <robert.fitzgerald@nrel.gov>
2025-06-12 09:53:56 -0600

    sketch constraints for multimodal frontier modeling

--- rust/bambam/src/model/frontier/mod.rs
+++ rust/bambam/src/model/frontier/mod.rs
@@ -1,1 +1,2 @@
 pub mod isochrone;
+pub mod multimodal;
---
+++ rust/bambam/src/model/frontier/multimodal/mod.rs
@@ -0,0 +1,7 @@
+mod multimodal_constraint;
+mod multimodal_constraint_config;
+mod multimodal_frontier_config;
+
+pub use multimodal_constraint::MultimodalConstraint;
+pub use multimodal_constraint_config::MultimodalConstraintConfig;
+pub use multimodal_frontier_config::MultimodalFrontierConfig;
