d6a0be9a34967abf04497636493ad3e8a5fd3879
rfitzger <robert.fitzgerald@nrel.gov>
2025-06-12 16:33:22 -0600

    explicit feature destination fieldnames

--- rust/bambam/src/model/traversal/multimodal/feature_dependency.rs
+++ rust/bambam/src/model/traversal/multimodal/feature_dependency.rs
@@ -9,17 +9,23 @@ use serde::{Deserialize, Serialize};
 #[serde(untagged, rename_all = "snake_case")]
 pub enum FeatureDependency {
     /// names an upstream feature that provides a time value
-    /// which will be added to the existing values for [`fieldname::EDGE_TIME`] and [`fieldname::TRIP_TIME`]
-    TimeDependency { time_feature: String },
+    /// which will be appended to the values in some destination(s)
+    TimeDependency {
+        time_feature: String,
+        destinations: Vec<String>,
+    },
     /// names an upstream feature that provides speed which can be used to compute a time value
     /// which will be added to the existing values for [`fieldname::EDGE_TIME`] and [`fieldname::TRIP_TIME`]
-    SpeedDependency { speed_feature: String },
+    SpeedDependency {
+        speed_feature: String,
+        destinations: Vec<String>,
+    },
     /// names an upstream feature that will be copied into another location.
     /// for example, in walk-mode trips with a penalty factor, this can be used to copy it over
     /// to a state feature named for cost aggregation.
     CustomFeatureCopy {
         source: String,
-        destination: String,
+        destinations: Vec<String>,
         unit: DependencyUnitType,
     },
 }
@@ -27,17 +33,13 @@ pub enum FeatureDependency {
 impl FeatureDependency {
     pub fn as_input_feature(&self) -> (String, InputFeature) {
         match self {
-            FeatureDependency::TimeDependency { time_feature } => {
+            FeatureDependency::TimeDependency { time_feature, .. } => {
                 (time_feature.clone(), InputFeature::Time(None))
             }
-            FeatureDependency::SpeedDependency { speed_feature } => {
+            FeatureDependency::SpeedDependency { speed_feature, .. } => {
                 (speed_feature.clone(), InputFeature::Speed(None))
             }
-            FeatureDependency::CustomFeatureCopy {
-                source,
-                destination,
-                unit,
-            } => (
+            FeatureDependency::CustomFeatureCopy { source, unit, .. } => (
                 source.clone(),
                 InputFeature::Custom {
                     name: source.clone(),
@@ -55,45 +57,61 @@ impl FeatureDependency {
     ) -> Result<(), StateModelError> {
         // get the time value based on the feature dependency
         match self {
-            FeatureDependency::TimeDependency { time_feature } => {
+            FeatureDependency::TimeDependency {
+                time_feature,
+                destinations,
+            } => {
                 let (time, time_unit) = state_model.get_time(state, time_feature, None)?;
-                state_model.add_time(state, fieldname::EDGE_TIME, &time, time_unit)?;
-                state_model.add_time(state, fieldname::TRIP_TIME, &time, time_unit)?;
+                for destination in destinations.iter() {
+                    state_model.add_time(state, destination, &time, time_unit)?;
+                }
                 Ok(())
             }
-            FeatureDependency::SpeedDependency { speed_feature } => {
+            FeatureDependency::SpeedDependency {
+                speed_feature,
+                destinations,
+            } => {
                 let (distance, distance_unit) =
                     state_model.get_distance(state, fieldname::EDGE_DISTANCE, None)?;
                 let (speed, speed_unit) = state_model.get_speed(state, speed_feature, None)?;
                 let (time, time_unit) =
                     Time::create((&distance, distance_unit), (&speed, speed_unit))?;
-                state_model.add_time(state, fieldname::EDGE_TIME, &time, &time_unit)?;
-                state_model.add_time(state, fieldname::TRIP_TIME, &time, &time_unit)?;
+                for destination in destinations.iter() {
+                    state_model.add_time(state, destination, &time, &time_unit)?;
+                }
                 Ok(())
             }
             FeatureDependency::CustomFeatureCopy {
                 source,
-                destination,
+                destinations,
                 unit,
             } => match unit {
                 DependencyUnitType::FloatingPoint => {
                     let value = state_model.get_custom_f64(state, source)?;
-                    state_model.set_custom_f64(state, destination, &value)?;
+                    for destination in destinations.iter() {
+                        state_model.set_custom_f64(state, destination, &value)?;
+                    }
                     Ok(())
                 }
                 DependencyUnitType::SignedInteger => {
                     let value = state_model.get_custom_i64(state, source)?;
-                    state_model.set_custom_i64(state, destination, &value)?;
+                    for destination in destinations.iter() {
+                        state_model.set_custom_i64(state, destination, &value)?;
+                    }
                     Ok(())
                 }
                 DependencyUnitType::UnsignedInteger => {
                     let value = state_model.get_custom_f64(state, source)?;
-                    state_model.set_custom_f64(state, destination, &value)?;
+                    for destination in destinations.iter() {
+                        state_model.set_custom_f64(state, destination, &value)?;
+                    }
                     Ok(())
                 }
                 DependencyUnitType::Boolean => {
                     let value = state_model.get_custom_bool(state, source)?;
-                    state_model.set_custom_bool(state, destination, &value)?;
+                    for destination in destinations.iter() {
+                        state_model.set_custom_bool(state, destination, &value)?;
+                    }
                     Ok(())
                 }
             },
