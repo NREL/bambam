332210da8fff454479e304a992018a69e2cc243c
rfitzger <robert.fitzgerald@nrel.gov>
2025-07-24 14:49:30 -0600

    overlay script refactor

--- rust/bambam/src/app/overlay/app.rs
+++ rust/bambam/src/app/overlay/app.rs
@@ -1,17 +1,228 @@
+//! script to aggregate mep output rows to some overlay geometry dataset.
+//! the number of output rows is not dependent on the size of the source geometry dataset,
+//! instead based on the number of geometry rows with matches in the mep dataset.
+//! only mep score and population data are aggregated at this time, via summation.
 use super::OverlayOperation;
-use crate::util::polygonal_rtree::PolygonalRTree;
+use crate::{app::overlay::OverlaySource, util::polygonal_rtree::PolygonalRTree as PrtBambam};
+use bamsoda_core::model::identifier::Geoid;
 use csv::StringRecord;
 use geo::Geometry;
 use itertools::Itertools;
-use kdam::{tqdm, Bar};
+use kdam::{tqdm, Bar, BarBuilder, BarExt};
 use rayon::prelude::*;
+use routee_compass_core::util::geo::PolygonalRTree;
 use serde::{Deserialize, Serialize};
 use std::{
     collections::HashMap,
+    fs::File,
+    io::{BufRead, Read},
     path::Path,
     sync::{Arc, Mutex},
 };
-use wkt::TryFromWkt;
+use wkt::{ToWkt, TryFromWkt};
+
+// CSV rows as currently defined:
+// grid_id,isochrone_10,isochrone_20,isochrone_30,isochrone_40,lat,lon,
+// mep,mep_entertainment,mep_food,mep_healthcare,mep_jobs,mep_retail,mep_services,
+// mode,opps_entertainment_10,opps_entertainment_20,opps_entertainment_30,opps_entertainment_40,opps_entertainment_total,
+// opps_food_10,opps_food_20,opps_food_30,opps_food_40,opps_food_total,
+// opps_healthcare_10,opps_healthcare_20,opps_healthcare_30,opps_healthcare_40,opps_healthcare_total,
+// opps_jobs_10,opps_jobs_20,opps_jobs_30,opps_jobs_40,opps_jobs_total,
+// opps_retail_10,opps_retail_20,opps_retail_30,opps_retail_40,opps_retail_total,opps_services_10,
+// opps_services_20,opps_services_30,opps_services_40,opps_services_total,
+// ram_mb,runtime_iter_opps,runtime_mep,runtime_opps,runtime_search
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct MepRow {
+    pub grid_id: String,
+    pub lat: f64,
+    pub lon: f64,
+    pub mep: f64,
+    pub mep_entertainment: f64,
+    pub mep_food: f64,
+    pub mep_healthcare: f64,
+    pub mep_jobs: f64,
+    pub mep_retail: f64,
+    pub mep_services: f64,
+    pub population: f64, // currently missing from rows
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct OutRow {
+    pub geoid: Geoid,
+    pub geometry: String,
+    pub mep: f64,
+    pub mep_entertainment: f64,
+    pub mep_food: f64,
+    pub mep_healthcare: f64,
+    pub mep_jobs: f64,
+    pub mep_retail: f64,
+    pub mep_services: f64,
+    pub population: f64, // currently missing from rows
+}
+
+impl OutRow {
+    pub fn new(geoid: Geoid, geometry: &Geometry, rows: &[MepRow]) -> Self {
+        let mut out_row = OutRow::empty(geoid.clone(), geometry);
+        for row in rows.iter() {
+            out_row.add(row);
+        }
+        out_row
+    }
+
+    /// sets up the OutRow with empty accumulators
+    pub fn empty(geoid: Geoid, geometry: &Geometry) -> Self {
+        OutRow {
+            geoid,
+            geometry: geometry.to_wkt().to_string(),
+            mep: Default::default(),
+            mep_entertainment: Default::default(),
+            mep_food: Default::default(),
+            mep_healthcare: Default::default(),
+            mep_jobs: Default::default(),
+            mep_retail: Default::default(),
+            mep_services: Default::default(),
+            population: Default::default(),
+        }
+    }
+
+    pub fn add(&mut self, mep_row: &MepRow) {
+        self.mep += mep_row.mep;
+        self.mep_entertainment += mep_row.mep_entertainment;
+        self.mep_food += mep_row.mep_food;
+        self.mep_healthcare += mep_row.mep_healthcare;
+        self.mep_jobs += mep_row.mep_jobs;
+        self.mep_retail += mep_row.mep_retail;
+        self.mep_services += mep_row.mep_services;
+        self.population += mep_row.population;
+    }
+}
+
+/// runs a
+pub fn run2(
+    mep_filepath: &str,
+    output_filepath: &str,
+    overlay_source: &OverlaySource,
+    how: &OverlayOperation,
+    chunksize: usize,
+) -> Result<(), String> {
+    // fail early if IO error from read/write destinations
+    let mep_result_file =
+        File::open(mep_filepath).map_err(|e| format!("error reading '{mep_filepath}': {e}"))?;
+    let mut output_writer = csv::Writer::from_path(output_filepath)
+        .map_err(|e| format!("failure opening output file '{}': {}", output_filepath, e))?;
+
+    // read overlay dataset
+    let overlay_data = overlay_source.build()?;
+    let overlay_lookup = overlay_data
+        .iter()
+        .map(|(geom, geoid)| (geoid.clone(), geom.clone()))
+        .collect::<HashMap<_, _>>();
+    let overlay: Arc<PolygonalRTree<f64, Geoid>> = Arc::new(PolygonalRTree::new(overlay_data)?);
+
+    // Read chunks of CSV rows at a time. the mep output can be very large on the order of 10s of GBs.
+    let mut bar = BarBuilder::default()
+        .desc("chunking mep data rows")
+        .position(0)
+        .build()?;
+    let mut chunk = String::new();
+    let mut chunks = 0;
+    let mut lines_read = 0;
+    let mut buf_reader = std::io::BufReader::new(mep_result_file);
+    let mut grouped: HashMap<Geoid, (Geometry, Vec<MepRow>)> = HashMap::new();
+    loop {
+        // Read a chunk of lines into `chunk`
+        chunk.clear();
+        chunks += 1;
+        let _ = bar.update(1);
+        let mut lines = buf_reader.by_ref().lines().take(chunksize);
+        let mut any = false;
+        while let Some(line) = lines.next() {
+            let line = line.map_err(|e| format!("error reading line: {e}"))?;
+            chunk.push_str(&line);
+            chunk.push('\n');
+            lines_read += 1;
+            any = true;
+        }
+        if !any {
+            // done reading from the buffer, all rows processed
+            break;
+        }
+
+        // read the next chunk, process it, and append the grouped collection
+        let mut reader = csv::Reader::from_reader(chunk.as_bytes());
+        let rows = reader
+            .deserialize()
+            .collect::<Result<Vec<MepRow>, _>>()
+            .map_err(|e| format!("failure deserializing chunk {chunks}: {e}"))?;
+        let tagged_rows: Vec<(Geoid, MepRow)> =
+            match_chunk(rows, output_filepath, overlay.clone())?;
+        for (geoid, row) in tagged_rows.into_iter() {
+            match grouped.get_mut(&geoid) {
+                Some((_, v)) => v.push(row),
+                None => {
+                    let geometry = overlay_lookup.get(&geoid).ok_or_else(|| {
+                        format!("internal error, lookup missing geometry entry for geoid '{geoid}'")
+                    })?;
+                    let _ = grouped.insert(geoid, (geometry.clone(), vec![row]));
+                }
+            }
+        }
+    }
+
+    // aggregate results
+    let result = grouped
+        .into_iter()
+        .map(|(geoid, (geometry, mep_rows))| OutRow::new(geoid, &geometry, &mep_rows))
+        .collect_vec();
+
+    for row in result.into_iter() {
+        output_writer
+            .serialize(row)
+            .map_err(|e| format!("failure writing row to output: {e}"))?;
+    }
+
+    println!("written to {output_filepath}");
+    Ok(())
+}
+
+fn match_chunk(
+    rows: Vec<MepRow>,
+    output_filename: &str,
+    overlay: Arc<PolygonalRTree<f64, Geoid>>,
+) -> Result<Vec<(Geoid, MepRow)>, String> {
+    let bar = Arc::new(Mutex::new(
+        BarBuilder::default()
+            .position(1)
+            .desc("spatial lookup")
+            .total(rows.len())
+            .build()?,
+    ));
+    rows.into_par_iter()
+        .flat_map(|row| {
+            if let Ok(mut b) = bar.clone().lock() {
+                let _ = b.update(1);
+            }
+            let point = geo::Geometry::Point(geo::Point::new(row.lon, row.lat));
+            let intersection_result = overlay.intersection(&point);
+            let found = match intersection_result {
+                Err(e) => return vec![Err(e)],
+                Ok(found) => found.collect_vec(),
+            };
+            match found[..] {
+                [] => vec![],
+                [single] => vec![Ok((single.data.clone(), row))],
+                _ => {
+                    let found_geoids = found.iter().map(|r| r.data.to_string()).join(", ");
+                    vec![Err(format!(
+                        "point {} unexpectedly found multiple geoids: [{}]",
+                        point.to_wkt().to_string(),
+                        found_geoids
+                    ))]
+                }
+            }
+        })
+        .collect::<Result<Vec<_>, _>>()
+}
 
 /// aggregate a bambam output to some other geospatial dataset via some overlay operation.
 ///
@@ -58,7 +269,7 @@ pub fn run(
             Ok((geometry, r.clone()))
         })
         .collect::<Result<Vec<_>, String>>()?;
-    let bambam_rtree = Arc::new(PolygonalRTree::new(bambam_geometries)?);
+    let bambam_rtree = Arc::new(PrtBambam::new(bambam_geometries)?);
 
     // read in overlay geometries file
     let overlay_path = Path::new(overlay_filepath);
--- rust/bambam/src/app/overlay/mod.rs
+++ rust/bambam/src/app/overlay/mod.rs
@@ -1,7 +1,9 @@
 mod aggregation_function;
 mod app;
 mod overlay_operation;
+mod overlay_source;
 
 pub use aggregation_function::AggregationFunction;
 pub use app::run;
 pub use overlay_operation::OverlayOperation;
+pub use overlay_source::OverlaySource;
