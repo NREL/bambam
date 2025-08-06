5fd96563f3d53388f73987a442484f8b040181c1
Rob Fitzgerald <robert.fitzgerald@nrel.gov>
2025-08-06 16:50:00 -0600

    connectivity test

--- rust/bambam-osm/src/main.rs
+++ rust/bambam-osm/src/main.rs
@@ -93,10 +93,20 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
+    use bambam_osm::model::osm::graph::{OsmNodeDataSerializable, OsmWayDataSerializable};
+    use routee_compass_core::util::fs::read_utils;
+
     #[test]
-    #[allow(unused)]
     fn test_e2e_liechtenstein() {
         // uses a small OSM dataset to test the end-to-end data processing
+        let cleanup_tmp_dir= match std::env::var("CLEANUP_TMP_DIR") {
+            Ok(value) => {
+                value.parse::<bool>().expect("CLEANUP_TMP_DIR must be 'true' or 'false'")
+            },
+            Err(_) => true, // default
+        }; 
+        let temp_directory = "src/test/tmp";
         let pbf_file = "src/test/liechtenstein-latest.osm.pbf";
         let extent_file = "src/test/schaan_liechtenstein.txt";
         let config_file = "../../configuration/bambam-osm/test_osm_import.toml";
@@ -104,13 +114,41 @@ mod tests {
             pbf_file: pbf_file.to_string(),
             extent_file: Some(extent_file.to_string()),
             configuration_file: Some(config_file.to_string()),
-            output_directory: "tmp".to_string(),
+            output_directory: temp_directory.to_string(),
         };
 
         if let Err(e) = crate::run(&conf) {
-            panic!("bambam-osm test failed: {e}");
-        } else {
-            std::fs::remove_dir_all("tmp").expect("failed to remove tmp directory");
+            panic!("bambam-osm run failure during import: {e}");
+        } 
+
+        // test graph connectivity
+        let mut connectivity_is_ok = true;
+        let ways_result: Result<Box<[OsmWayDataSerializable]>, _> = read_utils::from_csv(&"src/test/tmp/edges-complete.csv.gz", true, None, None);
+        let nodes_result: Result<Box<[OsmNodeDataSerializable]>, _> = read_utils::from_csv(&"src/test/tmp/vertices-complete.csv.gz", true, None, None);       
+        let invariant_error_msg = match (&ways_result, &nodes_result) {
+            (Ok(_), Ok(_)) => None,
+            (Ok(_), Err(e)) => Some(format!("failed to read nodes file: {e}")),
+            (Err(e), Ok(_)) => Some(format!("failed to read ways file: {e}")),
+            (Err(e1), Err(e2)) => Some(format!("failed to read both nodes and ways files: {e1} {e2}")),
+        };
+        if let (Ok(ways), Ok(nodes)) = (ways_result, nodes_result) {
+            let lookup = nodes.iter().enumerate().collect::<HashMap<_, _>>();
+            for way in ways.iter() {
+                if lookup.get(&way.src_vertex_id.0).is_none() || lookup.get(&way.dst_vertex_id.0).is_none() {
+                    connectivity_is_ok = false;
+                }
+            }
+        }
+
+        // cleanup before exits, order is important.
+        if cleanup_tmp_dir {
+            std::fs::remove_dir_all(temp_directory).expect("failed to remove tmp directory");
+        }
+        // error if we couldn't read the output files or if the graph connectivity was invalid.
+        match invariant_error_msg {
+            None if !connectivity_is_ok => panic!("files were written but some ways had invalid src/dst vertex ids"),
+            Some(msg) => panic!("{msg}"),
+            _ => {}
         }
     }
 }
