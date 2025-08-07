e5590c73c6d2e6a67ca38350e944d0689b6e8890
Rob Fitzgerald <robert.fitzgerald@nrel.gov>
2025-08-07 11:51:28 -0600

    minimally-reproducing error for CSV linestring deserialization

--- rust/bambam-osm/src/model/osm/graph/osm_way_ops.rs
+++ rust/bambam-osm/src/model/osm/graph/osm_way_ops.rs
@@ -274,4 +274,21 @@ mod tests {
             Err(e) => panic!("{e}"),
         }
     }
+    
+    #[test]
+    fn linestring_from_csv_01() {
+        #[derive(serde::Deserialize, Debug)]
+        struct Row {
+            index: usize,
+            #[serde(deserialize_with="super::deserialize_linestring")]
+            geometry: geo::LineString<f32>,
+        }
+        let path = "src/model/osm/graph/test/linestring_01.csv";
+        let mut row_reader = csv::Reader::from_path(&path).expect("test invariant: file should exist");
+        let rows = row_reader.deserialize().collect::<Result<Vec<Row>, _>>().expect("deserialization failed");
+        match &rows[..] {
+            [row] => println!("{:?}", row),
+            _ => panic!("unexpected rows result {:?}", rows)
+        }
+    }
 }
