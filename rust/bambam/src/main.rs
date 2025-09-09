1dbca8743142dbb20ebdb40a202044b35585b8e8
Rob Fitzgerald <robert.fitzgerald@nrel.gov>
2025-09-09 12:49:54 -0600

    app runner returning rows

--- rust/bambam/src/main.rs
+++ rust/bambam/src/main.rs
@@ -1,7 +1,12 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
+
 use bambam::model::builder;
 use clap::Parser;
 use routee_compass::app::cli::{cli_args::CliArgs, run};
-use routee_compass::app::compass::CompassAppError;
+use routee_compass::app::compass::{CompassApp, CompassAppError, CompassJsonExtensions};
+use serde_json::Value;
 inventory::submit! { builder::BUILDER_REGISTRATION }
 
 // Import the library to ensure inventory registrations in lib.rs are included
@@ -24,10 +29,16 @@ fn main() {
     }
 }
 
-fn run_bambam(args: CliArgs) -> Result<(), CompassAppError> {
+fn run_bambam(args: CliArgs) -> Result<Vec<Value>, CompassAppError> {
     log::info!("starting app at {}", chrono::Local::now().to_rfc3339());
-
-    run::command_line_runner(&args, None, None)
+    let config_path = Path::new(&args.config_file);
+    let app = CompassApp::try_from(config_path)?;
+    let query_filename = &args.query_file;
+    let query_file = File::open(query_filename).map_err(|e| CompassAppError::BuildFailure(format!("failure reading input query file '{query_filename}': {e}")))?;
+    let reader = BufReader::new(query_file);
+    let user_json: serde_json::Value = serde_json::from_reader(reader)?;
+    let mut user_queries = user_json.get_queries()?;
+    app.run(&mut user_queries, None)
 }
 
 #[cfg(test)]
@@ -69,7 +80,14 @@ mod test {
         };
 
         match run_bambam(args) {
-            Ok(()) => {}
+            Ok(rows) => {
+                assert_eq!(rows.len(), 144);
+                for (idx, row) in rows.iter().enumerate() {
+                    if let Some(error) = row.get("error") {
+                        panic!("row {idx} has error: {}", serde_json::to_string_pretty(error).unwrap_or_default());
+                    }
+                }
+            },
             Err(e) => panic!("test failed: {e}"),
         }
     }
