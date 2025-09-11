0de8a15221a2b64d3b85b5543d147b4728e4e134
Rob Fitzgerald <robert.fitzgerald@nrel.gov>
2025-09-11 16:45:54 -0600

    format

--- rust/bambam/src/model/label/multimodal_label_service.rs
+++ rust/bambam/src/model/label/multimodal_label_service.rs
@@ -1,5 +1,11 @@
+use std::sync::Arc;
+
 use routee_compass_core::model::{
-    label::{label_model_error::LabelModelError, label_model_service::LabelModelService, Label},
+    label::{
+        label_model_error::{self, LabelModelError},
+        label_model_service::LabelModelService,
+        Label, LabelModel,
+    },
     network::VertexId,
     state::{StateModel, StateVariable},
 };
@@ -10,11 +16,8 @@ impl LabelModelService for MultimodalLabelService {
     fn build(
         &self,
         query: &serde_json::Value,
-        state_model: std::sync::Arc<routee_compass_core::model::state::StateModel>,
-    ) -> Result<
-        std::sync::Arc<dyn routee_compass_core::model::label::LabelModel>,
-        routee_compass_core::model::label::label_model_error::LabelModelError,
-    > {
+        state_model: Arc<StateModel>,
+    ) -> Result<std::sync::Arc<dyn LabelModel>, label_model_error::LabelModelError> {
         todo!()
     }
 }
