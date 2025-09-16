e84a2d925658e1c4c166513396c88985b824cb22
Rob Fitzgerald <robert.fitzgerald@nrel.gov>
2025-09-16 16:08:32 -0600

    begin writing tests for trip leg model

--- rust/bambam/src/model/state/multimodal_trip_leg_model.rs
+++ rust/bambam/src/model/state/multimodal_trip_leg_model.rs
@@ -17,7 +17,6 @@ use uom::si::f64::{Length, Time};
 pub struct MultimodalTripLegModel {
     pub mode: String,
     pub max_trip_legs: u64,
-    pub state_model: Arc<StateModel>,
     pub mode_mapping: Arc<MultimodalMapping<String, i64>>,
     pub route_id_mapping: Arc<MultimodalMapping<String, i64>>,
 }
@@ -52,9 +51,9 @@ impl AccessModel for MultimodalTripLegModel {
         state_model: &StateModel,
     ) -> Result<(), AccessModelError> {
         // grab the leg_idx and leg mode if it exists
-        let leg_and_mode_opt = match self.get_active_leg_idx(state)? {
+        let leg_and_mode_opt = match self.get_active_leg_idx(state, state_model)? {
             Some(leg_idx) => {
-                let mode = self.get_leg_mode(state, leg_idx)?;
+                let mode = self.get_leg_mode(state, leg_idx, state_model)?;
                 Some((leg_idx, mode))
             }
             None => None,
@@ -66,8 +65,8 @@ impl AccessModel for MultimodalTripLegModel {
             }
             _ => {
                 // no leg assigned or a change in mode -> add the new leg
-                let next_leg_idx = self.increment_active_leg_idx(state)?;
-                self.set_leg_mode(state, next_leg_idx, &self.mode)?;
+                let next_leg_idx = self.increment_active_leg_idx(state, state_model)?;
+                self.set_leg_mode(state, next_leg_idx, &self.mode, state_model)?;
             }
         };
 
@@ -142,20 +141,22 @@ impl TraversalModel for MultimodalTripLegModel {
         state_model: &StateModel,
     ) -> Result<(), TraversalModelError> {
         let (_, edge, _) = trajectory;
-        let leg_idx = self.get_active_leg_idx(state)?.ok_or_else(|| {
-            multimodal_state_ops::error_inactive_state_traversal(state, &self.state_model)
-        })?;
+        let leg_idx = self
+            .get_active_leg_idx(state, state_model)?
+            .ok_or_else(|| {
+                multimodal_state_ops::error_inactive_state_traversal(state, state_model)
+            })?;
         let distance: Length = state_model.get_distance(state, fieldname::EDGE_DISTANCE)?;
         let time: Time = state_model.get_time(state, fieldname::EDGE_TIME)?;
-        let mode = self.get_leg_mode(state, leg_idx)?;
+        let mode = self.get_leg_mode(state, leg_idx, state_model)?;
         let d_leg = fieldname::leg_distance_fieldname(leg_idx);
         let d_mode = fieldname::mode_distance_fieldname(mode);
         let t_leg = fieldname::leg_distance_fieldname(leg_idx);
         let t_mode = fieldname::mode_time_fieldname(mode);
-        self.state_model.add_distance(state, &d_leg, &distance)?;
-        self.state_model.add_distance(state, &d_mode, &distance)?;
-        self.state_model.add_time(state, &t_leg, &time)?;
-        self.state_model.add_time(state, &t_mode, &time)?;
+        state_model.add_distance(state, &d_leg, &distance)?;
+        state_model.add_distance(state, &d_mode, &distance)?;
+        state_model.add_time(state, &t_leg, &time)?;
+        state_model.add_time(state, &t_mode, &time)?;
         Ok(())
     }
 
@@ -172,14 +173,27 @@ impl TraversalModel for MultimodalTripLegModel {
 }
 
 impl MultimodalTripLegModel {
+    pub fn new(
+        mode: String,
+        max_trip_legs: u64,
+        mode_mapping: Arc<MultimodalMapping<String, i64>>,
+        route_id_mapping: Arc<MultimodalMapping<String, i64>>,
+    ) -> MultimodalTripLegModel {
+        Self {
+            mode,
+            max_trip_legs,
+            mode_mapping,
+            route_id_mapping,
+        }
+    }
+
     /// inspect the current active leg for a trip
     pub fn get_active_leg_idx(
         &self,
         state: &[StateVariable],
+        state_model: &StateModel,
     ) -> Result<Option<LegIdx>, StateModelError> {
-        let leg_i64 = self
-            .state_model
-            .get_custom_i64(state, fieldname::ACTIVE_LEG)?;
+        let leg_i64 = state_model.get_custom_i64(state, fieldname::ACTIVE_LEG)?;
         if leg_i64 < 0 {
             Ok(None)
         } else {
@@ -197,10 +211,11 @@ impl MultimodalTripLegModel {
         &self,
         state: &mut [StateVariable],
         leg_idx: LegIdx,
+        state_model: &StateModel,
     ) -> Result<bool, StateModelError> {
         self.validate_leg_idx(leg_idx)?;
         let name = fieldname::leg_mode_fieldname(leg_idx);
-        let label = self.state_model.get_custom_i64(state, &name)?;
+        let label = state_model.get_custom_i64(state, &name)?;
         Ok(label >= 0)
     }
 
@@ -209,18 +224,16 @@ impl MultimodalTripLegModel {
         &self,
         state: &[StateVariable],
         leg_idx: LegIdx,
+        state_model: &StateModel,
     ) -> Result<&str, StateModelError> {
         self.validate_leg_idx(leg_idx)?;
         let name = fieldname::leg_mode_fieldname(leg_idx);
-        let label = self.state_model.get_custom_i64(state, &name)?;
+        let label = state_model.get_custom_i64(state, &name)?;
         if label < 0 {
             Err(StateModelError::RuntimeError(format!(
-                "get_leg_mode called on empty leg with id {leg_idx}"
-            )))
-        } else if label >= self.max_trip_legs as i64 {
-            Err(StateModelError::RuntimeError(format!(
-                "get_leg_mode called with leg id {leg_idx} >= max leg id {}",
-                self.max_trip_legs
+                "Internal Error: get_leg_mode called on leg idx {} but mode label is not set (stored as {})",
+                leg_idx,
+                label
             )))
         } else {
             self.mode_mapping
@@ -239,27 +252,30 @@ impl MultimodalTripLegModel {
         &self,
         state: &mut [StateVariable],
         leg_idx: LegIdx,
+        state_model: &StateModel,
     ) -> Result<Length, StateModelError> {
         let name = fieldname::leg_distance_fieldname(leg_idx);
-        self.state_model.get_distance(state, &name)
+        state_model.get_distance(state, &name)
     }
 
     pub fn get_leg_time(
         &self,
         state: &[StateVariable],
         leg_idx: LegIdx,
+        state_model: &StateModel,
     ) -> Result<Time, StateModelError> {
         let name = fieldname::leg_time_fieldname(leg_idx);
-        self.state_model.get_time(state, &name)
+        state_model.get_time(state, &name)
     }
 
     pub fn get_leg_route_id(
         &self,
         state: &[StateVariable],
         leg_idx: LegIdx,
+        state_model: &StateModel,
     ) -> Result<Option<&String>, StateModelError> {
         let name = fieldname::leg_route_id_fieldname(leg_idx);
-        let route_id_label = self.state_model.get_custom_i64(state, &name)?;
+        let route_id_label = state_model.get_custom_i64(state, &name)?;
         let route_id = self.route_id_mapping.get_categorical(route_id_label)?;
         Ok(route_id)
     }
@@ -283,9 +299,10 @@ impl MultimodalTripLegModel {
     pub fn increment_active_leg_idx(
         &self,
         state: &mut [StateVariable],
+        state_model: &StateModel,
     ) -> Result<LegIdx, StateModelError> {
         // get the index of the next leg
-        let next_leg_idx_u64 = match self.get_active_leg_idx(state)? {
+        let next_leg_idx_u64 = match self.get_active_leg_idx(state, state_model)? {
             Some(leg_idx) => {
                 let next = leg_idx + 1;
                 self.validate_leg_idx(next)?;
@@ -301,8 +318,7 @@ impl MultimodalTripLegModel {
         })?;
 
         // increment the value in the state vector
-        self.state_model
-            .set_custom_i64(state, fieldname::ACTIVE_LEG, &next_leg_idx)?;
+        state_model.set_custom_i64(state, fieldname::ACTIVE_LEG, &next_leg_idx)?;
         Ok(next_leg_idx_u64)
     }
 
@@ -313,11 +329,113 @@ impl MultimodalTripLegModel {
         state: &mut [StateVariable],
         leg_idx: LegIdx,
         mode: &str,
+        state_model: &StateModel,
     ) -> Result<(), StateModelError> {
         let mode_label = self.mode_mapping.get_label(mode).ok_or_else(|| {
             StateModelError::RuntimeError(format!("mode mapping has no entry for '{}' mode", mode))
         })?;
         let name = fieldname::leg_mode_fieldname(leg_idx);
-        self.state_model.set_custom_i64(state, &name, mode_label)
+        state_model.set_custom_i64(state, &name, mode_label)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::sync::Arc;
+
+    use crate::model::state::{
+        fieldname, multimodal_trip_leg_model::MultimodalTripLegModel, MultimodalMapping,
+    };
+    use routee_compass_core::model::{
+        access::AccessModel,
+        network::{Edge, Vertex},
+        state::{StateModel, StateVariable},
+    };
+    use uom::si::f64::Length;
+
+    // an initialized trip that has not begun should have active leg of None and
+    // leg_0_mode of None.
+    #[test]
+    fn test_initialize_trip_access() {
+        let mode_mapping = Arc::new(
+            MultimodalMapping::new(&[String::from("bike"), String::from("walk")])
+                .expect("test invariant failed: unable to construct mapping"),
+        );
+        let route_id_mapping = Arc::new(
+            MultimodalMapping::new(&[String::from("1")])
+                .expect("test invariant failed: unable to construct mapping"),
+        );
+        let this_mode = "walk".to_string();
+        let mmm = MultimodalTripLegModel::new(
+            this_mode.clone(),
+            1,
+            mode_mapping.clone(),
+            route_id_mapping,
+        );
+        let state_model = StateModel::new(mmm.state_features());
+        let mut state = state_model
+            .initial_state()
+            .expect("test invariant failed: unable to create state");
+
+        let result_idx = mmm
+            .get_active_leg_idx(&state, &state_model)
+            .expect("failure getting active leg index");
+
+        // we have no accessor for the mode, but can confirm, the state should have
+        // two variables, both set to the EMPTY value of -1.0.
+        assert_eq!(state, vec![StateVariable(-1.0), StateVariable(-1.0)]);
+    }
+
+    // in a scenario with walk and bike mode, using an AccessModel for walk mode,
+    // if we start a trip, we should assign 'walk' to the first leg and the active
+    // leg should be 0.
+    #[test]
+    fn test_start_trip_access() {
+        let mode_mapping = Arc::new(
+            MultimodalMapping::new(&[String::from("bike"), String::from("walk")])
+                .expect("test invariant failed: unable to construct mapping"),
+        );
+        let route_id_mapping = Arc::new(
+            MultimodalMapping::new(&[String::from("1")])
+                .expect("test invariant failed: unable to construct mapping"),
+        );
+        let test_mode = "walk".to_string();
+        let mmm = MultimodalTripLegModel::new(
+            test_mode.clone(),
+            1,
+            mode_mapping.clone(),
+            route_id_mapping,
+        );
+        let state_model = StateModel::new(mmm.state_features());
+
+        let trajectory = (
+            &Vertex::new(0, 0.0, 0.0),
+            &Edge::new(0, 0, 0, 1, Length::new::<uom::si::length::meter>(1000.0)),
+            &Vertex::new(1, 0.01, 0.0),
+            &Edge::new(0, 1, 1, 2, Length::new::<uom::si::length::meter>(1000.0)),
+            &Vertex::new(2, 0.02, 0.0),
+        );
+        let mut state = state_model
+            .initial_state()
+            .expect("test invariant failed: unable to create state");
+
+        mmm.access_edge(trajectory, &mut state, &state_model)
+            .expect("access failed");
+
+        let json = &state_model
+            .serialize_state(&state, false)
+            .expect("unable to serialize state");
+
+        let active_leg = mmm
+            .get_active_leg_idx(&state, &state_model)
+            .expect("failure getting active leg index")
+            .expect("active leg is not set");
+
+        let leg_0_mode = mmm
+            .get_leg_mode(&state, active_leg, &state_model)
+            .expect(&format!("failure getting mode for leg {active_leg}"));
+
+        assert_eq!(active_leg, 0);
+        assert_eq!(leg_0_mode, &test_mode);
     }
 }
