ed2ceddcdc24675f03b77612a1f211ba9a191c55
Rob Fitzgerald <robert.fitzgerald@nrel.gov>
2025-09-23 10:13:25 -0600

    comment on metric-based constraints

--- rust/bambam/src/model/frontier/multimodal/constraint_config.rs
+++ rust/bambam/src/model/frontier/multimodal/constraint_config.rs
@@ -8,10 +8,13 @@ pub enum MultimodalFrontierConstraintConfig {
     AllowedModes(Vec<String>),
     ModeCounts(HashMap<String, NonZeroU64>),
     TripLegCount(NonZeroU64),
+    ExactSequences(Vec<Vec<String>>),
+    // TODO: these metric-based constraints need to be run in the traversal model where those
+    // metrics are being evaluated. but in order to produce "is invalid" they would need to
+    // be able to set the Cost to infinity. perhaps by setting Time::MAX?
     // DistanceLimit(HashMap<String, DistanceTuple>),
     // TimeLimit(HashMap<String, TimeTuple>),
     // EnergyLimit(HashMap<String, EnergyTuple>),
-    ExactSequences(Vec<Vec<String>>),
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
