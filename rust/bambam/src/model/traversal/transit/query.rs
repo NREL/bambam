d2ab1e767b0a9acd59b94291e9c7c49ce1fd277f
Yamil Essus <Yamil.Essus@nrel.gov>
2025-10-14 10:31:28 -0400

    Fix datetime deserialization

--- rust/bambam/src/model/traversal/transit/query.rs
+++ rust/bambam/src/model/traversal/transit/query.rs
@@ -3,9 +3,21 @@ use serde::de::Error;
 use serde::{Deserialize, Deserializer, Serialize};
 use uom::si::f64::Time;
 
+pub const APP_DATETIME_FORMAT: &str = "%Y-%m-%d %H:%M:%S";
+
+pub fn deserialize_naive_datetime<'de, D>(deserializer: D) -> Result<NaiveDateTime, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let date_str: String = String::deserialize(deserializer)?;
+    chrono::NaiveDateTime::parse_from_str(&date_str, APP_DATETIME_FORMAT)
+        .map_err(|e| D::Error::custom(format!("Invalid datetime format: {}", e)))
+}
+
 #[derive(Serialize, Deserialize)]
 pub struct TransitTraversalQuery {
-    pub start_datetime: NaiveDateTime,
+    #[serde(deserialize_with = "deserialize_naive_datetime")]
+    pub start_datetime: NaiveDateTime, // Fix deserialization
     /// If true, we maintain a DWELL_TIME state variable
     pub record_dwell_time: Option<bool>,
 }
