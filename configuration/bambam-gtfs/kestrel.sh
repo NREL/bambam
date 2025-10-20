05ecabdb4706a471e2cc6dfb7ba669b9d109993c
Rob Fitzgerald <robert.fitzgerald@nrel.gov>
2025-10-20 11:19:26 -0600

    HPC run template for national bambam-gtfs

--- configuration/bambam-gtfs/kestrel.sh
+++ configuration/bambam-gtfs/kestrel.sh
@@ -14,10 +14,12 @@ module load bzip2
     --input /home/$USER/data/bam/gtfs/2025-10-08 \
     --starting-edge-list-id 1 \
     --parallelism 50 \
-    --output-directory "/projects/mepcore/data/out/rfitzger/2025-10-15-gtfs" \
+    --output-directory "/projects/mepcore/data/out/rfitzger/2025-10-17-gtfs" \
     --vertices-compass-filename "/projects/mepcore/lib/routee-compass-tomtom/data/tomtom_national/vertices-complete.csv.gz" \
-    --start-date 09-01-2025 \
-    --end-date 09-01-2025 \
-    --date-mapping-policy nearest-date \
-    --date-mapping-date-tolerance 365 \
+    --start-date 09-01-2023 \
+    --end-date 09-01-2023 \
+    --start-time 08:00:00 \
+    --end-time 09:00:00 \
+    --date-mapping-policy nearest-date-time-range \
+    --date-mapping-date-tolerance 730 \
     --date-mapping-match-weekday true \
