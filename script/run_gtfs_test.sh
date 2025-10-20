0bfba7faaf56791fbe85f9e010c4625f3bb4cfc4
Rob Fitzgerald <robert.fitzgerald@nrel.gov>
2025-10-20 11:17:32 -0600

    filename change

--- script/run_gtfs_test.sh
+++ script/run_gtfs_test.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "running bambam-gtfs to generate Compass datasets and metadata from a GTFS archive"
-./configuration/bambam-gtfs/local-match-nearest.sh  
+./configuration/bambam-gtfs/local-match-nearest-date.sh  
 echo "running gtfs-config to modify an existing BAMBAM TOML configuration with this transit dataset"
 ./rust/target/release/bambam_util gtfs-config --directory boulder_co/transit --base-config configuration/test_gtfs_config_boulder.toml
 echo "running BAMBAM with a walk-transit trip"
