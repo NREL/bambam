2d0b062fcdcce1a68367a95aac742cf68c7625d9
Rob Fitzgerald <robert.fitzgerald@nrel.gov>
2025-10-23 13:50:02 -0600

    use new Compass CLI runner in main

--- rust/bambam/src/main.rs
+++ rust/bambam/src/main.rs
@@ -5,7 +5,9 @@ use std::path::Path;
 use bambam::model::builder;
 use clap::Parser;
 use routee_compass::app::cli::cli_args::CliArgs;
-use routee_compass::app::compass::{CompassApp, CompassAppError, CompassJsonExtensions};
+use routee_compass::app::compass::{
+    CompassApp, CompassAppError, CompassBuilderInventory, CompassJsonExtensions,
+};
 use serde_json::Value;
 inventory::submit! { builder::BUILDER_REGISTRATION }
 
@@ -23,35 +25,25 @@ fn main() {
     //     chunksize: None,
     //     newline_delimited: false,
     // };
-    match run_bambam(args) {
+    let builder = CompassBuilderInventory::new().expect("failed to load compass app builder");
+    match routee_compass::app::cli::run::command_line_runner(&args, Some(builder), None) {
         Ok(_) => {}
         Err(e) => log::error!("{e}"),
     }
 }
 
-fn run_bambam(args: CliArgs) -> Result<Vec<Value>, CompassAppError> {
-    log::info!("starting app at {}", chrono::Local::now().to_rfc3339());
-    let config_path = Path::new(&args.config_file);
-    let app = CompassApp::try_from(config_path)?;
-    let query_filename = &args.query_file;
-    let query_file = File::open(query_filename).map_err(|e| {
-        CompassAppError::BuildFailure(format!(
-            "failure reading input query file '{query_filename}': {e}"
-        ))
-    })?;
-    let reader = BufReader::new(query_file);
-    let user_json: serde_json::Value = serde_json::from_reader(reader)?;
-    let mut user_queries = user_json.get_queries()?;
-    app.run(&mut user_queries, None)
-}
-
 #[cfg(test)]
 mod test {
-    use std::path::PathBuf;
-
-    use routee_compass::app::cli::cli_args::CliArgs;
-
-    use crate::run_bambam;
+    use routee_compass::app::{
+        cli::cli_args::CliArgs,
+        compass::{CompassApp, CompassAppError, CompassJsonExtensions},
+    };
+    use serde_json::Value;
+    use std::{
+        fs::File,
+        io::BufReader,
+        path::{Path, PathBuf},
+    };
 
     #[test]
     fn test_e2e_denver() {
@@ -84,7 +76,7 @@ mod test {
         };
         let expected_grid_size = 48;
 
-        match run_bambam(args) {
+        match test_run_bambam(args) {
             Ok(rows) => {
                 assert_eq!(rows.len(), expected_grid_size);
                 for (idx, row) in rows.iter().enumerate() {
@@ -98,5 +90,27 @@ mod test {
             }
             Err(e) => panic!("test failed: {e}"),
         }
+    }
+
+    /// runs bambam for test cases and expects a Vec<Value> result.
+    /// <foot-shooting-clause>
+    ///     this test runner is only here because there is no equivalent runner
+    ///     in routee-compass. it also makes the assumption that the query dataset is
+    ///     NOT newline-delimited.
+    /// </foot-shooting-clause>
+    fn test_run_bambam(args: CliArgs) -> Result<Vec<Value>, CompassAppError> {
+        log::info!("starting app at {}", chrono::Local::now().to_rfc3339());
+        let config_path = Path::new(&args.config_file);
+        let app = CompassApp::try_from(config_path)?;
+        let query_filename = &args.query_file;
+        let query_file = File::open(query_filename).map_err(|e| {
+            CompassAppError::BuildFailure(format!(
+                "failure reading input query file '{query_filename}': {e}"
+            ))
+        })?;
+        let reader = BufReader::new(query_file);
+        let user_json: serde_json::Value = serde_json::from_reader(reader)?;
+        let mut user_queries = user_json.get_queries()?;
+        app.run(&mut user_queries, None)
     }
 }
