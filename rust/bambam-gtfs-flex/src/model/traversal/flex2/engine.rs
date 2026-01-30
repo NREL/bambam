398168e9045fe9d28c9fb373e79e088d0f47c2e6
Rob Fitzgerald <robert.fitzgerald@nrel.gov>
2026-01-29 17:09:06 -0700

    wire zone graph into engine

--- rust/bambam-gtfs-flex/src/model/traversal/flex2/engine.rs
+++ rust/bambam-gtfs-flex/src/model/traversal/flex2/engine.rs
@@ -1,8 +1,13 @@
+use crate::model::traversal::{flex::ZoneId, flex2::zone_graph::ZoneGraph};
+
 use super::Flex2Config;
 
-use routee_compass_core::model::traversal::TraversalModelError;
+use routee_compass_core::{model::traversal::TraversalModelError, util::geo::PolygonalRTree};
 
-pub struct Flex2Engine {}
+pub struct Flex2Engine {
+    pub graph: ZoneGraph,
+    pub rtree: PolygonalRTree<f64, ZoneId>,
+}
 
 impl TryFrom<Flex2Config> for Flex2Engine {
     type Error = TraversalModelError;
--- rust/bambam-gtfs-flex/src/model/traversal/flex2/zone_graph/mod.rs
+++ rust/bambam-gtfs-flex/src/model/traversal/flex2/zone_graph/mod.rs
@@ -3,6 +3,7 @@ mod zonal_relation;
 mod zone_id;
 mod zone_record;
 
+pub use graph::ZoneGraph;
 pub use zonal_relation::ZonalRelation;
 pub use zone_id::ZoneId;
 pub use zone_record::ZoneRecord;
