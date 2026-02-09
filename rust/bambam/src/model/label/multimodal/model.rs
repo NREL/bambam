a1842c3abfaa10221224799df6037ad9fe69ef47
Rob Fitzgerald <robert.fitzgerald@nrel.gov>
2026-02-09 09:20:12 -0700

    debug logging in multimodal components

--- rust/bambam/src/model/label/multimodal/model.rs
+++ rust/bambam/src/model/label/multimodal/model.rs
@@ -1,5 +1,6 @@
 //! builds labels that include enumerations for leg modes.
 //!
+use itertools::Itertools;
 use routee_compass_core::model::{
     label::{label_model_error::LabelModelError, Label, LabelModel},
     network::VertexId,
@@ -51,9 +52,9 @@ impl LabelModel for MultimodalLabelModel {
         let label = Label::new_u8_state(vertex_id, &mode_labels)?;
 
         log::debug!(
-            "multimodal label model at vertex {} produced label {} for state at time: {:.2} minutes",
+            "multimodal label model at vertex {} produced label [{}] for state at time: {:.2} minutes",
             vertex_id,
-            label,
+            mode_labels.iter().map(|l| self.mode_to_state.get_categorical(*l as i64).unwrap_or_default().cloned().unwrap_or_default()).join("->"),
             state_model
                 .get_time(state, "trip_time")
                 .unwrap_or_default()
